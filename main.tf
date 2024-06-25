963b10521231bc50a09435d1cba91b7d7f86c1b8
GraceMichelle <gracemichelle105@gmail.com>
2024-06-25 10:48:40 +0700

    add teraformbaru

--- main.tf
+++ main.tf
@@ -6,9 +6,12 @@ resource "aws_instance" "notesproject" {
   ami           = "ami-003c463c8207b4dfa"
   instance_type = "t2.micro"
   key_name      = "docker.pem"
-  
 
   tags = {
     Name = "jenkins-server"
   }
+}
+
+output "public_ip" {
+  value = aws_instance.notesproject.public_ip
 }
